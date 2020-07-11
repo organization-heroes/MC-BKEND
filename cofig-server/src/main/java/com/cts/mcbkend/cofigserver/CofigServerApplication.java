9d16706482858972e12730cf5fbe09a15279d1ab
unknown <sukantaban1993@gmail.com>
2020-07-12 01:36:06 +0530

    Externalize configuration added

--- cofig-server/src/main/java/com/cts/mcbkend/cofigserver/CofigServerApplication.java
+++ cofig-server/src/main/java/com/cts/mcbkend/cofigserver/CofigServerApplication.java
@@ -1,15 +1,17 @@
-package com.cts.mcbkend.userservice;
+package com.cts.mcbkend.cofigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class CofigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(CofigServerApplication.class, args);
 	}
 
 }
--- h2db-initializer/src/main/java/com/cts/mcbkend/h2dbinitializer/H2dbInitializerApplication.java
+++ h2db-initializer/src/main/java/com/cts/mcbkend/h2dbinitializer/H2dbInitializerApplication.java
@@ -2,13 +2,19 @@ package com.cts.mcbkend.h2dbinitializer;
 
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class H2dbInitializerApplication {
+	
+	@Value("${custom.h2.tcpport}")
+	private String tcpPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2dbInitializerApplication.class, args);
@@ -16,6 +22,6 @@ public class H2dbInitializerApplication {
 
 	@Bean(initMethod = "start", destroyMethod = "stop")
     public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", tcpPort);
     }
 }
